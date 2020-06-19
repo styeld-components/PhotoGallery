1118a290c485c904414cfca02a7c352d944d9e2d
Sejin Park <sejinpark0209@gmail.com>
2020-06-18 23:13:31 -0700

    create prev button on GalleryDetail with functionality

--- client/src/Components/GalleryDetail.jsx
+++ client/src/Components/GalleryDetail.jsx
@@ -9,6 +9,7 @@ class GalleryDetail extends React.Component {
     };
     this.closeClickHandler = this.closeClickHandler.bind(this);
     this.nextClickHandler = this.nextClickHandler.bind(this);
+    this.prevClickHandler = this.prevClickHandler.bind(this);
   }
 
   closeClickHandler() {
@@ -19,19 +20,39 @@ class GalleryDetail extends React.Component {
     this.setState({ currPhotoIdx: this.state.currPhotoIdx + 1 });
   }
 
+  prevClickHandler() {
+    this.setState({ currPhotoIdx: this.state.currPhotoIdx - 1 });
+  }
+
   render() {
     // let currPhotoIdx = 1;
     // let currImage = this.props.photos.room_photos[4].imageUrl;
+    let prevBtn;
+    let nextBtn;
+    if(this.state.currPhotoIdx === 0) {
+      prevBtn = null;
+    } else {
+      prevBtn = <button className="prevBtn" onClick={this.prevClickHandler}>prev</button>
+    }
+
+    if(this.state.currPhotoIdx === this.props.photos.room_photos.length - 1) {
+      nextBtn = null;
+    } else {
+      nextBtn = <button className="nextBtn" onClick={this.nextClickHandler}>next</button>
+    }
 
     return (
       <div>
         <button className="closeBtn" onClick={this.closeClickHandler}>Close</button>
     <span>{this.state.currPhotoIdx + 1 + ' / ' + this.props.photos.room_photos.length}</span>
-        <div className="gallery-container">
-          <div className="gallery-item">
-            <div className="image">
+        <div className="gallery-container-detail">
+          <div className="gallery-item-detail">
+            <div className="image-detail">
+              {/* <button className="prevBtn" onClick={this.prevClickHandler}>prev</button> */}
+              {prevBtn}
               <img src={this.props.photos.room_photos[this.state.currPhotoIdx].imageUrl} />
-              <button onClick={this.nextClickHandler}>next</button>
+              {/* <button className="nextBtn" onClick={this.nextClickHandler}>next</button> */}
+              {nextBtn}
             </div>
           </div>
         </div>
