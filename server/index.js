18a22c58c2dc9c7ebc623b5e4a3856adbf390631
Sejin Park <sejinpark0209@gmail.com>
2020-06-24 19:22:04 -0700

    modify image resizing and style next prev button on GalleryDetail component

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3004;
+const port = 3005;
 const bodyParser = require('body-parser');
 const Controllers = require('./Controllers.js');
 
